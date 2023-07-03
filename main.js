7f0bf506731e47dfbc22e4a76daee992861da605
Muhammad Rifqi <muhammad45rifki@gmail.com>
2023-07-03 15:40:37 +0700

    Update main.js
    Update Main.Js

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600
-    })
-  
-    win.loadFile('index.html')
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
   }
-
-  app.whenReady().then(() => {
-    createWindow()
-  })
-
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
+})
